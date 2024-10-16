be0f6849c538efe107d198f3424a8d64b4f6031c
Leon <leonhinds1986@gmail.com>
2024-10-15 22:14:12 -0400

    Worked on POST and GET request

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,14 +1,11 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 from django.urls import path
 
-from .views import get_notes
+from .views import get_notes, CustomTokenObtainPairView, CustomRefreshTokenView, logout, is_authenticated
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('notes/', get_notes)
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CustomRefreshTokenView.as_view(), name='token_refresh'),
+    path('notes/', get_notes),
+    path('logout/', logout),
+    path('authenticated/', is_authenticated)
 ]
